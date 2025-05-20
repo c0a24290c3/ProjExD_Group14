ae6e4f08cb727e413c19b763c8302bf64fd34b5c
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-05-20 17:49:23 +0900

    hp&score

--- kokaton_ran.py
+++ kokaton_ran.py
@@ -5,10 +5,40 @@ import pygame as pg
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+class Score:
+    #スコアの表示
+    def __init__(self):
+        self.font = pg.font.Font(None, 50)
+        self.color = (0, 200, 100)
+        self.value = 0
+        self.image = self.font.render(f"Score: {self.value}", 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (100,80)
+
+    def update(self, screen: pg.Surface):
+        self.image = self.font.render(f"Score: {self.value}", 0, self.color)
+        screen.blit(self.image, self.rect)
+
+class Health:
+    #HPの表示
+    def __init__(self):
+        self.font = pg.font.Font(None, 50)
+        self.color = (255,0,255)
+        self.health = 100 #初期HP
+        self.image = self.font.render(f"HP: {self.health}", 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (100,40)
+    
+    def update(self, screen: pg.Surface):
+        self.image = self.font.render(f"HP: {self.health}", 0, self.color)
+        screen.blit(self.image, self.rect)
+
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    score=Score()
+    health=Health()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)#練習8
     kk_img = pg.image.load("fig/3.png") #練習2
@@ -19,6 +49,21 @@ def main():
     tmr = 0
     M = kk_rct.move_ip
     while True:
+        if tmr % 10 == 0:
+            health.health -=1
+            score.value+=1 #タイマー10ごとにスコア1加算
+        if health.health==0: #HPが0になったときgameover
+            score_image = score.font.render(f"Score: {score.value}", 0, score.color)
+            score_rect = score_image.get_rect()
+            score_rect.center=400, 400
+            screen.blit(score_image,score_rect)
+            gmfo=pg.font.Font(None,150)
+            gmov=gmfo.render(f"GAME OVER",0, (0,0,0))
+            gmov_rect=gmov.get_rect()
+            gmov_rect.center=(400, 200)
+            screen.blit(gmov,gmov_rect)
+            pg.display.update()
+            return
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
@@ -39,8 +84,10 @@ def main():
         screen.blit(bg_img2, [-X+1600,0])#練習7,8
         screen.blit(bg_img, [-X+3200,0])#練習9
         screen.blit(kk_img, kk_rct) #練習4,10-5
+        score.update(screen)
+        health.update(screen)
         pg.display.update()
-        tmr += 1        
+        tmr += 1       
         clock.tick(200) #練習5
 
 if __name__ == "__main__":
